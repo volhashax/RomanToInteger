a388971aca281ab028ea56c5f3bd2d13c2360e84
volhashax <volhashax@gmail.com>
2021-12-12 20:54:27 +0300

    method is added

--- RomanToIntegerApp/Program.cs
+++ RomanToIntegerApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RomanToIntegerApp
 {
@@ -6,7 +7,34 @@ namespace RomanToIntegerApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите римское число:");
+            string roman = Console.ReadLine();
+            
+            int number = 0;
+            for (int i = 0; i < roman.Length; i++)
+            {
+                if (i + 1 < roman.Length && RomanMap[roman[i]] < RomanMap[roman[i + 1]])
+                {
+                    number -= RomanMap[roman[i]];
+                }
+                else
+                {
+                    number += RomanMap[roman[i]];
+                }
+            }
+            Console.WriteLine(number);
+
         }
+
+        private static Dictionary<char, int> RomanMap = new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
     }
 }
